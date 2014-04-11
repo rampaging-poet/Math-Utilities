5dbcd70ea2df1c40c7b5ffa39a93c8c60adc6168
rampaging-poet <rampaging_poet@yahoo.com>
2014-04-11 11:08:18 -0700

    Coefficient array is now private. Added tests of subtraction. Fixed subtraction

--- apmathpolynomial_test.pas
+++ apmathpolynomial_test.pas
@@ -34,9 +34,11 @@ type
   TTestPolynomials= class(TTestCase)
   published
     procedure TestArrayAssignment;
+    procedure TestIndividualAssignment;
     procedure TestValue;
     procedure TestEqual;
     procedure TestAdd;
+    procedure TestSubtract;
     procedure TestMultiply;
   end;
 
@@ -48,20 +50,7 @@ var
   TestArray: TExtendedArray;
   I: Integer;
 begin
-  //Tests of implicit assignment of dynamic arrays to polynomials.
-  TestArray := nil;
-  a := TestArray;
-  Assert(a.Coefficients = nil,'Non-nil coefficients after assigning nil array.');
-
-  //All-zero trim test.
-  SetLength(TestArray,5);
-  for I := 0 to 4 do
-    TestArray[I] := 0;
-
-  a := TestArray;
-  Assert((Length(a.Coefficients) = 1) and (a.Coefficients[0] = 0),
-         'Failed to trim all-zero array.');
-
+  //Test trimming when assigning a dynamic array.
   TestArray := nil;
   SetLength(TestArray,5);
   TestArray[0] := 1;
@@ -70,8 +59,8 @@ begin
 
   a := TestArray;
 
-  Assert(Length(a.Coefficients) = 1, 'Failed to trim array on assignment.');
-  Assert(a.Coefficients[0] = 1,      'Trimmed array does not have correct values.');
+  Assert(a.Order = 0,           'Failed to trim array on assignment.');
+  Assert(a.Coefficients[0] = 1, 'Trimmed array does not have correct values.');
 
   //Non-trim test.
   TestArray := nil;
@@ -80,23 +69,47 @@ begin
     TestArray[I] := I;
 
   a := TestArray;
-  Assert(Length(a.Coefficients) = 5,
-         'Trimmed array when array did not have trailing zeroes.');
+  Assert(a.Order = 4, 'Trimmed array when array did not have trailing zeroes.');
 
-  //Ensure copy-on-write semantics
-  Assert(a.Coefficients <> TestArray, 'Polynomial contains a direct reference to TestArray.');
+  //Check values
+  for I := 0 to 4 do
+    Assert(a.Coefficients[I] = I, 'Unexpected value after dynamic array assignment.');
+
+  //Test assignment by SetCoefficients
+  SetCoefficients(a, [1,2,3]);
+  Assert(a.Coefficients[0] = 1, 'Unexpected value after open array assignment.');
+  Assert(a.Coefficients[1] = 2, 'Unexpected value after open array assignment.');
+  Assert(a.Coefficients[2] = 3, 'Unexpected value after open array assignment.');
+end;
+
+procedure TTestPolynomials.TestIndividualAssignment;
+var
+  a: TPolynomial;
+begin
+  //Assign a value past the end of the array.
+  a := nil;
+  a.Coefficients[2] := 1;
+
+  Assert(a.Order = 2, 'Unexpected order after assigning coefficient.');
+  Assert(a.Coefficients[0] = 0, 'Unexpected value at index 0 after assigning coefficient.');
+  Assert(a.Coefficients[1] = 0, 'Unexpected value at index 1 after assigning coefficient.');
+  Assert(a.Coefficients[2] = 1, 'Unexpected value at index 2 after assigning coefficient.');
+
+  //Ensure we trim correctly after assigning 0.
+  a.Coefficients[2] := 0;
+  Assert(a.Order = 0, 'Failed to reduce order after assigning 0 to highest-order term.');
 end;
 
 procedure TTestPolynomials.TestValue;
 var
   a: TPolynomial;
 begin
+  a := nil;
+
   //Ensure value of nil is zero for some arbitrary value of x.
-  a.Coefficients := nil;
   Assert(a.Value(137) = 0, 'Value of nil polynomial is not zero.');
 
   //Ensure value of 0th-order polynomials is a constant.
-  SetLength(a.Coefficients,1);
   a.Coefficients[0] := 1;
 
   Assert(a.Value(0)   = 1, 'Value of constant polynomial is not constant.');
@@ -104,10 +117,7 @@ begin
   Assert(a.Value(137) = 1, 'Value of constant polynomial is not constant.');
 
   //Ensure value of higher-order polynomials is correctly calculated.
-  SetLength(a.Coefficients,3);
-  a.Coefficients[0] := 1;
-  a.Coefficients[1] := 2;
-  a.Coefficients[2] := 3;
+  SetCoefficients(a, [1,2,3]);
 
   Assert(a.Value(0) = 1,       'Unexpected value of polynomial 3x^2 + 2x + 1');
   Assert(a.Value(7) = 162,     'Unexpected value of polynomial 3x^2 + 2x + 1');
@@ -119,67 +129,41 @@ var
   a, b: TPolynomial;
 begin
   //Ensure equals works for nil arrays.
-  a.Coefficients := nil;
-  b.Coefficients := nil;
-
+  a := nil;
+  b := nil;
   Assert(a = b, 'Nil polynomials not considered equal.');
 
+  //Ensure nil is not equal to a real polynomial.
+  SetCoefficients(a, [1,2,3]);
+  if b = a then Assert(false, 'Nil = a real polynomial.');
+  if a = b then Assert(false, 'A real polynomial = nil.');
+
   //Ensure equals command works between identical arrays.
-  SetLength(a.Coefficients, 3);
-  a.Coefficients[0] := 1;
-  a.Coefficients[1] := 2;
-  a.Coefficients[2] := 3;
-  b.Coefficients := a.Coefficients;
+  SetCoefficients(b, [1,2,3]);
 
   Assert(a = b, 'Identical arrays not considered equal.');
-
-  //Ensure equals command works between copied arrays.
-  b.Coefficients := Copy(a.Coefficients,0,MaxInt);
-  Assert(a = b, 'Copy of identical array not considered equal to original');
-
-  //Ensure trailing zeroes don't affect equality.
-  SetLength(a.Coefficients,5);
-  a.Coefficients[3] := 0;
-  a.Coefficients[4] := 0;
-
-  Assert(a = b, 'Trailing zeroes prevented equality - a > b.');
-  Assert(b = a, 'Trailing zeroes prevented equality - a < b.');
-
-  //Ensure nil is not equal to a real polynomial.
-  a.Coefficients := nil;
-  if a = b then Assert(false, 'Nil = a real polynomial.');
-  if b = a then Assert(false, 'A real polynomial = nil.');
 end;
 
 procedure TTestPolynomials.TestAdd;
 var
   a, b, c: TPolynomial;
 begin
+  a := nil;
+  b := nil;
+  c := nil;
   //Check addition between nil = nil.
-  a.Coefficients := nil;
-  b.Coefficients := nil;
-  c.Coefficients := nil;
-
   Assert(a + b = c, 'Nil + Nil <> Nil.');
 
   //Check addition between nil and real.
-  SetLength(a.Coefficients,3);
-  a.Coefficients[0] := 1;
-  a.Coefficients[1] := 2;
-  a.Coefficients[2] := 3;
+  SetCoefficients(a, [1,2,3]);
+  SetCoefficients(c, [1,2,3]);
 
-  c.Coefficients := a.Coefficients;
   Assert(a + b = c, 'Real polynomial + nil <> original polynomial.');
   Assert(b + a = c, 'Nil + real polynomial <> original polynomial.');
 
   //Check addition between two real polynomials.
-  b.Coefficients := a.Coefficients;
-
-  c.Coefficients := nil;
-  SetLength(c.Coefficients,3);
-  c.Coefficients[0] := 2;
-  c.Coefficients[1] := 4;
-  c.Coefficients[2] := 6;
+  SetCoefficients(b, [1,2,3]);
+  SetCoefficients(c, [2,4,6]);
 
   Assert(a + a = c,     'Unexpected sum of polynomials - same order.');
 
@@ -187,52 +171,66 @@ begin
   Assert(a + b = b + a, 'Addition did not commute - same order.');
 
   //Check addition with non-equal orders.
-  a.Coefficients := Copy(a.Coefficients,0,MaxInt);
-  SetLength(a.Coefficients,5);
-  a.Coefficients[3] := 4;
   a.Coefficients[4] := 5;
+  a.Coefficients[3] := 4;
 
-  SetLength(c.Coefficients,5);
-  c.Coefficients[3] := 4;
   c.Coefficients[4] := 5;
+  c.Coefficients[3] := 4;
 
   Assert(a + b = b + a, 'Addition did not commute - different order.');
   Assert(a + b = c, 'Unexpected sum of polynomials - different order.');
+end;
+
+procedure TTestPolynomials.TestSubtract;
+var
+  a, b, c: TPolynomial;
+begin
+  a := nil;
+  b := nil;
+  c := nil;
+
+  //Ensure -nil = nil
+  Assert(a = -a, 'Negative nil was not equal to nil.');
+
+  //Ensure nil - nil = nil
+  Assert(a - b = c, 'Nil minus nil was not equal to nil.');
+
+  //Ensure negative works properly for real polynomials.
+  SetCoefficients(a, [1,2,3]);
+  SetCoefficients(c, [-1,-2,-3]);
+
+  Assert(-a = c, 'Unexpected value of unary subtraction operator.');
+  Assert(-c = a, 'Unexpected value of unary subtraction operator.');
+
+  //Ensure real polynomial less nil is original polynomial.
+  Assert(a - b = a,  'Subtracting nil not equivalent to subtracting zero.');
+  Assert(b - a = -a, 'Subtracting from nil not equivalent to subtracting from zero.');
+
+  //Check subtraction of real polynomials.
+  SetCoefficients(b, [2, 4, 6]);
+  Assert(a - b = c, 'Unexpected value of binary subtraction.');
 end;
 
 procedure TTestPolynomials.TestMultiply;
 var
   a, b, c: TPolynomial;
 begin
-  b.Coefficients := nil;
-  c.Coefficients := nil;
-
   //Check multipliaction with nil.
-  SetLength(a.Coefficients, 3);
-  a.Coefficients[0] := 1;
-  a.Coefficients[1] := 2;
-  a.Coefficients[2] := 3;
+  SetCoefficients(a, [1,2,3]);
+  b := nil;
+  c := nil;
 
   Assert(a * b = c, 'Multiplying by nil does not equal nil.');
   Assert(b * a = c, 'Multiplying by nil does not equal nil.');
 
   //Check multiplication of two real coefficients.
-  b.Coefficients := a.Coefficients;
-
-  SetLength(c.Coefficients,5);
-  c.Coefficients[0] := 1;
-  c.Coefficients[1] := 4;
-  c.Coefficients[2] := 10;
-  c.Coefficients[3] := 12;
-  c.Coefficients[4] := 9;
+  SetCoefficients(b, [1,2,3]);
+  SetCoefficients(c, [1,4,10,12,9]);
 
   Assert(a * b = c, 'Unexpected result of multiplication.');
 
   //Check multiplication by a constant.
-  SetLength(c.Coefficients,3);
-  c.Coefficients[0] := 2;
-  c.Coefficients[1] := 4;
-  c.Coefficients[2] := 6;
+  SetCoefficients(c, [2,4,6]);
 
   Assert(2 * a = c, 'Unexpected result of multiplication by a real number.');
   Assert(a * 2 = c, 'Unexpected result of multiplication by a real number.');
--- apmathpolynomials.pas
+++ apmathpolynomials.pas
@@ -32,12 +32,17 @@ type
   TExtendedArray = array of Extended;
 
   TPolynomial = record
+  private
     //The ith value is the coefficient of the ith-order term of the polynomial.
-    Coefficients: TExtendedArray;
+    FCoefficients: TExtendedArray;
 
     procedure TrimCoefficients;
 
+    function  GetCoefficient(const Index: Integer): Extended;
+    procedure SetCoefficient(const Index: Integer; const Value: Extended);
+  public
     //Evaluation
+    function Order: Integer;
     function Value(x: Extended): Extended;
     function Derivative: TPolynomial;
     function Integral:   TPolynomial;
@@ -52,37 +57,93 @@ type
     class operator Multiply(a, b: TPolynomial): TPolynomial;           overload;
     class operator Multiply(a: Extended; b: TPolynomial): TPolynomial; overload;
     class operator Multiply(a: TPolynomial; b: Extended): TPolynomial; overload;
+  public
+    property Coefficients[const Index: Integer]: Extended read GetCoefficient
+      write SetCoefficient;
   end;
+
+  //Set all coefficients from an open array - allows assigning constants.
+  //Using an out parameter prevents "not initialized" warnings.
+  procedure SetCoefficients(out a: TPolynomial; const Values: array of Extended);
 
 implementation
 uses
   Math;
+
+resourcestring
+  FSInvalidIndex = 'Invalid polynomial index %d';
 
 procedure TPolynomial.TrimCoefficients;
 var
   I: Integer;
 begin
   //Do nothing if no coefficients are assigned.
-  if Coefficients = nil then Exit;
+  if FCoefficients = nil then Exit;
 
   //Do nothing if there are no trailing zeros.
-  if Coefficients[Pred(Length(Coefficients))] <> 0 then Exit;
+  if FCoefficients[High(FCoefficients)] <> 0 then Exit;
 
   //Trim trailing zeroes to ensure length and order are related.
-  for I := Pred(Pred(Length(Coefficients))) downto 0 do
+  for I := Pred(Pred(Length(FCoefficients))) downto 0 do
   begin
-    if Coefficients[I] <> 0 then
+    if FCoefficients[I] <> 0 then
     begin
       //At least one non-zero coefficient exists. Set Coefficients and exit.
-      Coefficients := Copy(Coefficients,0,Succ(I));
+      FCoefficients := Copy(FCoefficients,0,Succ(I));
       Exit;
     end;
   end;
 
   //If we get here, then the array was entirely composed of zeroes.
-  Coefficients := nil; //Remove any existing reference.
-  SetLength(Coefficients,1);
-  Coefficients[0] := 0;
+  FCoefficients := nil; //Remove any existing reference.
+  SetLength(FCoefficients,1);
+  FCoefficients[0] := 0;
+end;
+
+function TPolynomial.GetCoefficient(const Index: Integer): Extended;
+begin
+  Result := 0;
+
+  if (Index < 0) then
+    raise EInvalidArgument.CreateFmt(FSInvalidIndex,[Index]);
+
+  if (FCoefficients = nil) then Exit;
+  if Length(FCoefficients) <= Index then Exit;
+
+  Result := FCoefficients[Index];
+end;
+
+procedure TPolynomial.SetCoefficient(const Index: Integer; const Value: Extended);
+var
+  Len: Integer;
+  I: Integer;
+begin
+  if (Index < 0) then
+    raise EInvalidArgument.CreateFmt(FSInvalidIndex,[Index]);
+
+  if (FCoefficients = nil) then
+    Len := 0
+  else
+    Len := Length(FCoefficients);
+
+  if Index >= Len then
+  begin
+    //Attempt to write coefficient past end of array. Extend array.
+    SetLength(FCoefficients,Succ(Index));
+    for I := Len to Pred(Index) do
+      FCoefficients[I] := 0;
+  end;
+
+  FCoefficients[Index] := Value; //Assign given value.
+  TrimCoefficients;
+end;
+
+function TPolynomial.Order: Integer;
+begin
+  if FCoefficients = nil then
+    Result := 0
+  else
+    Result := Pred(Length(FCoefficients));
 end;
 
 function TPolynomial.Value(x: Extended): Extended;
@@ -91,8 +152,8 @@ var
 begin
   Result := 0;
 
-  for I := Pred(Length(Coefficients)) downto 0 do
-    Result := Result * x + Coefficients[I];
+  for I := Pred(Length(FCoefficients)) downto 0 do
+    Result := Result * x + FCoefficients[I];
 end;
 
 function TPolynomial.Derivative: TPolynomial;
@@ -100,26 +161,26 @@ var
   Len: Integer;
   I: Integer;
 begin
-  if Coefficients = nil then
+  if FCoefficients = nil then
   begin
-    Result.Coefficients := nil;
+    Result.FCoefficients := nil;
     Exit;
   end;
 
-  Len := Length(Coefficients);
+  Len := Length(FCoefficients);
   //Catch the base case - f(x) is a constant.
   if Len < 2 then
   begin
-    SetLength(Result.Coefficients, 1);
-    Result.Coefficients[0] := 0;
+    SetLength(Result.FCoefficients, 1);
+    Result.FCoefficients[0] := 0;
     Exit;
   end;
 
   //The result is a polynomial of degree n-1.
-  SetLength(Result.Coefficients, Pred(Len));
+  SetLength(Result.FCoefficients, Pred(Len));
 
   for I := 1 to Pred(Len) do
-    Result.Coefficients[Pred(I)] := Coefficients[I] * I;
+    Result.FCoefficients[Pred(I)] := FCoefficients[I] * I;
 end;
 
 function TPolynomial.Integral: TPolynomial;
@@ -127,26 +188,26 @@ var
   Len: Integer;
   I: Integer;
 begin
-  if Coefficients = nil then
+  if FCoefficients = nil then
   begin
-    Result.Coefficients := nil;
+    Result.FCoefficients := nil;
     Exit;
   end;
 
-  Len := Length(Coefficients);
+  Len := Length(FCoefficients);
 
   //Polynomial is of degree n+1, constant of integration assumed to be zero.
-  SetLength(Result.Coefficients,Succ(Len));
-  Result.Coefficients[0] := 0;
+  SetLength(Result.FCoefficients,Succ(Len));
+  Result.FCoefficients[0] := 0;
 
   //Integral of ith term is ith term divided by i+1.
   for I := 1 to Pred(Len) do
-    Result.Coefficients[I+1] := Coefficients[I] / (I + 1);
+    Result.FCoefficients[I+1] := FCoefficients[I] / (I + 1);
 end;
 
 class operator TPolynomial.Implicit(a: TExtendedArray): TPolynomial;
 begin
-  Result.Coefficients := Copy(a,0,MaxInt);
+  Result.FCoefficients := Copy(a,0,MaxInt);
   Result.TrimCoefficients;
 end;
 
@@ -154,10 +215,10 @@ class operator TPolynomial.Negative(a: TPolynomial): TPolynomial;
 var
   I: Integer;
 begin
-  Result.Coefficients := a.Coefficients;
+  Result.FCoefficients := Copy(a.FCoefficients,0,MaxInt);
 
-  for I := 0 to Pred(Length(Result.Coefficients)) do
-    Result.Coefficients[I] := -Result.Coefficients[I];
+  for I := 0 to Pred(Length(Result.FCoefficients)) do
+    Result.FCoefficients[I] := -Result.FCoefficients[I];
 end;
 
 class operator TPolynomial.Equal(a, b: TPolynomial): Boolean;
@@ -169,38 +230,38 @@ begin
   Result := False;
 
   //Nil checks.
-  if (a.Coefficients = nil) then
+  if (a.FCoefficients = nil) then
   begin
-    if (b.Coefficients = nil) then
+    if (b.FCoefficients = nil) then
       Result := True
     else
-      Result := (Length(b.Coefficients) = 1) and (b.Coefficients[0] = 0);
+      Result := (Length(b.FCoefficients) = 1) and (b.FCoefficients[0] = 0);
 
     Exit;
   end;
 
-  if (b.Coefficients = nil) then
+  if (b.FCoefficients = nil) then
   begin
-    Result := (Length(a.Coefficients) = 1) and (a.Coefficients[0] = 0);
+    Result := (Length(a.FCoefficients) = 1) and (a.FCoefficients[0] = 0);
     Exit;
   end;
 
   //Check order of polynomials.
-  Len := Length(a.Coefficients);
+  Len := Length(a.FCoefficients);
 
-  case CompareValue(Len, Length(b.Coefficients)) of
-    -1: LongArray := b.Coefficients; //a is shorter than b
-    0:  LongArray := nil;            //They are the same length
-    1:                               //a is longer than b
+  case CompareValue(Len, Length(b.FCoefficients)) of
+    -1: LongArray := b.FCoefficients; //a is shorter than b
+    0:  LongArray := nil;             //They are the same length
+    1:                                //a is longer than b
       begin
-        LongArray := a.Coefficients;
-        Len       := Length(b.Coefficients);
+        LongArray := a.FCoefficients;
+        Len       := Length(b.FCoefficients);
       end;
   end;
 
   //Iterate over polynomials to find first nonmatching coefficients.
   for I := 0 to Pred(Len) do
-    if a.Coefficients[I] <> b.Coefficients[I] then Exit;
+    if a.FCoefficients[I] <> b.FCoefficients[I] then Exit;
 
   //All coefficients match.  Check for additional terms in longer array.
   if Assigned(LongArray) then
@@ -227,30 +288,30 @@ var
   I: Integer;
 begin
   //Nil checks
-  if a.Coefficients = nil then
+  if a.FCoefficients = nil then
   begin
-    Result.Coefficients := b.Coefficients;
+    Result.FCoefficients := Copy(b.FCoefficients,0,MaxInt);
     Exit;
   end;
 
-  if b.Coefficients = nil then
+  if b.FCoefficients = nil then
   begin
-    Result.Coefficients := a.Coefficients;
+    Result.FCoefficients := Copy(a.FCoefficients,0,MaxInt);
     Exit;
   end;
 
   //Determine which polynomial is of higher degree.
-  MinLen := Length(a.Coefficients);
+  MinLen := Length(a.FCoefficients);
 
-  if Length(b.Coefficients) >= MinLen then
+  if Length(b.FCoefficients) >= MinLen then
   begin
-    SourceArray := a.Coefficients;
-    ResultArray := Copy(b.Coefficients,0,MaxInt);
+    SourceArray := a.FCoefficients;
+    ResultArray := Copy(b.FCoefficients,0,MaxInt);
   end else
   begin
-    MinLen := Length(b.Coefficients);
-    SourceArray := b.Coefficients;
-    ResultArray := Copy(a.Coefficients);
+    MinLen := Length(b.FCoefficients);
+    SourceArray := b.FCoefficients;
+    ResultArray := Copy(a.FCoefficients);
   end;
 
   //Add lower degree polynomial to higher higher degree ResultArray.
@@ -258,7 +319,7 @@ begin
     ResultArray[I] := ResultArray[I] + SourceArray[I];
 
   //Assign result array to result polynomial.
-  Result.Coefficients := ResultArray;
+  Result.FCoefficients := ResultArray;
   Result.TrimCoefficients;
 end;
 
@@ -275,28 +336,28 @@ var
   I, J: Integer;
 begin
   //Check for unassigned coefficients.
-  if (a.Coefficients = nil) or (b.Coefficients = nil) then
+  if (a.FCoefficients = nil) or (b.FCoefficients = nil) then
   begin
-    Result.Coefficients := nil;
+    Result.FCoefficients := nil;
     Exit;
   end;
 
   //Length = Degree + 1.
   //Degree(a*b) = Degree(a) + Degree(b)
-  //Therefore Length(a*b) = Length(a) + Length(b) - 1;
-  Len := Length(a.coefficients) + Length(b.coefficients) - 1;
+  //Therefore Length(a*b) = a.Order + b.Order +1;
+  Len := Succ(a.Order + b.Order);
   SetLength(ResultArray,Len);
   for I := 0 to Pred(Len) do
     ResultArray[I] := 0;
 
   //Results have been initialized to zero. Iterate over both source arrays.
-  for I := 0 to Pred(Length(a.Coefficients)) do
+  for I := 0 to Pred(Length(a.FCoefficients)) do
   begin
-    for J := 0 to Pred(Length(b.Coefficients)) do
-      ResultArray[I+J] := ResultArray[I+J] + a.Coefficients[I]*b.Coefficients[J];
+    for J := 0 to Pred(Length(b.FCoefficients)) do
+      ResultArray[I+J] := ResultArray[I+J] + a.FCoefficients[I]*b.FCoefficients[J];
   end;
 
-  Result.Coefficients := ResultArray;
+  Result.FCoefficients := ResultArray;
 end;
 
 class operator TPolynomial.Multiply(a: Extended; b:TPolynomial): TPolynomial;
@@ -305,19 +366,49 @@ var
 begin
   if a = 0 then
   begin
-    Result.Coefficients := nil;
-    SetLength(Result.Coefficients, 1);
-    Result.Coefficients[0] := 0;
+    Result.FCoefficients := nil;
+    SetLength(Result.FCoefficients, 1);
+    Result.FCoefficients[0] := 0;
   end;
 
-  SetLength(Result.Coefficients, Length(b.Coefficients));
-  for I := 0 to Pred(Length(Result.Coefficients)) do
-    Result.Coefficients[I] := a * b.Coefficients[I];
+  SetLength(Result.FCoefficients, Length(b.FCoefficients));
+  for I := 0 to Pred(Length(Result.FCoefficients)) do
+    Result.FCoefficients[I] := a * b.FCoefficients[I];
 end;
 
 class operator TPolynomial.Multiply(a: TPolynomial; b:Extended): TPolynomial;
 begin
   Result := b*a; //Polynomials and real numbers commute.
+end;
+
+procedure SetCoefficients(out a: TPolynomial; const Values: array of Extended);
+var
+  LastIndex: Integer;
+  I: Integer;
+begin
+  LastIndex := -1;
+
+  //Find the last non-zero entry.
+  for I := High(Values) downto Low(Values) do
+  begin
+    if Values[I] <> 0 then
+    begin
+      LastIndex := I;
+      Break;
+    end;
+  end;
+
+  if LastIndex < 0 then
+  begin
+    a.FCoefficients := nil;
+    Exit;
+  end;
+
+  //Replace existing array.
+  SetLength(a.FCoefficients, Succ(High(Values)));
+
+  for I := 0 to High(Values) do
+    a.FCoefficients[I] := Values[I];
 end;
 
 end.
